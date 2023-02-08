33564b60232fafc6e97b7e258c8d7f19c831aebc
TejalDhepe <121013655+TejalDhepe@users.noreply.github.com>
2023-02-08 15:55:37 +0530

    first commit

---
+++ src/repository/Test1.java
@@ -0,0 +1,13 @@
+package repository;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	int a=10,b=20;
+	int sum;
+	sum=a+b;
+	System.out.println("addition="+sum);
+
+	}
+
+}
---
+++ src/repository/TestRepo.java
@@ -0,0 +1,10 @@
+package repository;
+
+public class TestRepo {
+
+	public static void main(String[] args) {
+	System.out.println("first try with github");
+
+	}
+
+}
